cbe5d0193fa34bdd5fe048b4a09265ba60c87751
rbrenaget <romain.brenaget@hotmail.fr>
2020-03-21 20:34:47 +0100

    Update test

--- c-trivium/src/test.c
+++ c-trivium/src/test.c
@@ -1,12 +1,9 @@
 /**
  * @file test.c
  * @author Romain Brenaget
- * @brief 
+ * @brief Simple verification test, this is not unit test
  * @version 0.1
  * @date 2020-03-03
- * 
- * @copyright Copyright (c) 2020
- * 
  */
 
 #include <stdio.h>
@@ -14,29 +11,51 @@
 #include <trivium.h>
 
 
+void print_byte(const uint8_t byte) {
+    static const char hex[] = "0123456789ABCDEF";
+    char out[3];
+
+    out[0] = hex[(byte >> 4) & 0xf];
+    out[1] = hex[byte & 0xf];
+    out[2] = '\0';
+
+    printf("%s", out);
+}
+
+void print_uint32(const uint32_t value) {
+    print_byte((uint8_t)(value >> 24));
+    print_byte((uint8_t)(value >> 16));
+    print_byte((uint8_t)(value >> 8));
+    print_byte((uint8_t)value);
+}
+
+void debug_data(const uint32_t* data, uint32_t len) {
+
+    for (uint32_t i = 0; i < len; i++)
+        print_uint32(data[i]);
+
+    printf("\n");   
+}
+
+
 int main() {
     TRIVIUM_ctx ctx;
-    //const uint8_t key[10] = {0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa};
-    //const uint8_t iv[10] = {0xdb, 0x92, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    const uint8_t key[2] = {0x00, 0x00};
-    const uint8_t iv[2] = {0x00, 0x00};
-    int res;
+    uint32_t output[4] = {0};
 
-    res = TRIVIUM_init(&ctx, key, iv, sizeof(key), sizeof(iv));
+    //Set 6, vector# 3:
+    const uint8_t key[10] = {0xfa, 0xa7, 0x54, 0x01, 0xae, 0x5b, 0x08, 0xb5, 0x62, 0x0f};
+    const uint8_t iv[10] = {0xc7, 0x60, 0xf9, 0x92, 0x2b, 0xc4, 0x5d, 0xf6, 0x8f, 0x28};
 
-    if (res == 0) {
-        uint32_t output[4] = {0};
-        printf("Debug Init :\n");
-        debug_data(ctx.lfsr_a, 3);
-        debug_data(ctx.lfsr_b, 3);
-        debug_data(ctx.lfsr_c, 4);
-        TRIVIUM_keystream(&ctx, output, 2);
-        debug_data(output, 4);
+    // Set 2, vector# 0:
+    //const uint8_t key[10] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    //const uint8_t iv[10] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-    }
-    else {
-        printf("Something wrong !\n");
-    }
+    TRIVIUM_init(&ctx, key, iv, sizeof(key), sizeof(iv));
+
+    printf("Keystream :\n");
+    TRIVIUM_genkeystream32(&ctx, output, 4);
+    debug_data(output, 4);
+    print_uint32(output[0]);
 
     return 0;
 }
