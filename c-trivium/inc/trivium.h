49e1a9354f149fdbc162822a0e9bde39c43f8126
rbrenaget <romain.brenaget@hotmail.fr>
2020-03-22 12:52:57 +0100

    Make trivium as aa shared library

--- c-trivium/inc/trivium.h
+++ c-trivium/inc/trivium.h
@@ -17,21 +17,20 @@
 #define MAX_IV_LEN 10
 
 typedef struct {
-  uint8_t keylen;
-  uint8_t ivlen;
+    uint8_t keylen;
+    uint8_t ivlen;
 
-  uint32_t lfsr_a[3];
-  uint32_t lfsr_b[3];
-  uint32_t lfsr_c[4];
+    uint32_t lfsr_a[3];
+    uint32_t lfsr_b[3];
+    uint32_t lfsr_c[4];
 
-  uint8_t key[MAX_KEY_LEN];
-  uint8_t iv[MAX_IV_LEN];
-
+    uint8_t key[MAX_KEY_LEN];
+    uint8_t iv[MAX_IV_LEN];
 } TRIVIUM_ctx;
 
-void TRIVIUM_init(TRIVIUM_ctx* ctx, const uint8_t key[], const uint8_t iv[], uint8_t keylen, uint8_t ivlen);
-void TRIVIUM_keysetup(TRIVIUM_ctx* ctx, const uint8_t key[]);
-void TRIVIUM_ivsetup(TRIVIUM_ctx* ctx, const uint8_t iv[]);
-void TRIVIUM_genkeystream32(TRIVIUM_ctx* ctx, uint32_t output[], uint32_t n);
+extern void TRIVIUM_init(TRIVIUM_ctx* ctx, const uint8_t key[], const uint8_t iv[], uint8_t keylen, uint8_t ivlen);
+extern void TRIVIUM_keysetup(TRIVIUM_ctx* ctx, const uint8_t key[]);
+extern void TRIVIUM_ivsetup(TRIVIUM_ctx* ctx, const uint8_t iv[]);
+extern void TRIVIUM_genkeystream32(TRIVIUM_ctx* ctx, uint32_t output[], uint32_t n);
 
 #endif
