4c451a8aff4204b0a83689c7555d213c57d91f4f
rbrenaget <romain.brenaget@hotmail.fr>
2020-03-25 19:36:29 +0100

    Add method initialize()

--- pytrivium/trivium.py
+++ pytrivium/trivium.py
@@ -22,10 +22,10 @@ class Trivium(object):
         else:
             self.lib_trivium_path = lib_trivium_path
 
-        # 
+        # Instantiate cffi base object
         self.ffi = FFI()
 
-        # 
+        # Load Trivium header file
         with open(self.h_file_path) as h_file:
             out = ''
             for line in h_file.readlines():
@@ -36,12 +36,24 @@ class Trivium(object):
         # Load the shared library into ffi
         self.lib = self.ffi.dlopen(self.lib_trivium_path)
 
+        self.context = self.ffi.new('TRIVIUM_ctx*')
+
     def initialize(self, key, iv):
         """
         """
-        pass
+        # Get key and iv length
+        keylen = len(key)
+        ivlen = len(iv)
+        # Initialialize trivium
+        self.lib.TRIVIUM_init(
+            self.context,
+            self.ffi.new(f'uint8_t[{keylen}]', key),
+            self.ffi.new(f'uint8_t[{ivlen}]', iv),
+            self.ffi.cast('uint8_t', keylen),
+            self.ffi.cast('uint8_t', ivlen)
+        )
 
-    def update(self):
+    def update(self, n):
         """
         """
         pass
@@ -54,6 +66,10 @@ class Trivium(object):
 
 
 if __name__ == '__main__':
-    key = [0x0]
-    iv = [0x0]
+    # Set 6, vector# 3:
+    key = [0xfa, 0xa7, 0x54, 0x01, 0xae, 0x5b, 0x08, 0xb5, 0x62, 0x0f]
+    iv = [0xc7, 0x60, 0xf9, 0x92, 0x2b, 0xc4, 0x5d, 0xf6, 0x8f, 0x28]
+
     engine = Trivium()
+
+    engine.initialize(key, iv)
