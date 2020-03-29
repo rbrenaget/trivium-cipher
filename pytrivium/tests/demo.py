52ea96554f4c966cb8f8454fbeb41c2aad21885c
rbrenaget <romain.brenaget@hotmail.fr>
2020-03-29 16:55:16 +0200

    Update demo

--- pytrivium/tests/demo.py
+++ pytrivium/tests/demo.py
@@ -17,17 +17,19 @@ def measure(name=''):
 
 
 def test():
-    N = 1000000
+    N = 1
     # Set 6, vector# 3:
     key = [0xfa, 0xa7, 0x54, 0x01, 0xae, 0x5b, 0x08, 0xb5, 0x62, 0x0f]
     iv = [0xc7, 0x60, 0xf9, 0x92, 0x2b, 0xc4, 0x5d, 0xf6, 0x8f, 0x28]
 
-    engine_api = Trivium()
-    engine_api.initialize(key, iv)
+    engine = Trivium()
+    engine.initialize(key, iv)
 
     with measure('API'):
         for _ in range(N):
-            engine_api.update(1)
+            engine.update(1)
+        
+        print([hex(r) for r in engine.finalize()])
 
 
 if __name__ == '__main__':
