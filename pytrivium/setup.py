b92d920f6a2acfb994b32bf6fa29733b6ff310c9
rbrenaget <romain.brenaget@hotmail.fr>
2020-05-29 20:03:13 +0200

    New version 1.0.7

--- pytrivium/setup.py
+++ pytrivium/setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pytrivium',
-    version='1.0.6',
+    version='1.0.7',
     author='Romain Brenaget',
 
     description='Python bindings to a C implementation of Trivium',
